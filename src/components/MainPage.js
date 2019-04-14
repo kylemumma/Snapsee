89b0e065f2e747de78b504533b12426794fae52d
Kyle Mumma <kyle.mumma@live.com>
2019-04-14 09:21:39 -0700

    sfdfds

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -98,7 +98,7 @@ class MainPage extends React.Component {
         Request.post(this.api('image/match/'))
             .field('username', this.state.snap_username)
             .attach('image', this.state.imageToClassify)
-            .then((res) => {alert(res)});
+            .then((res) => {console.log(res)});
     }
 
     render(){
